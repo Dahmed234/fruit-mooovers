051a1be8824bdd240777cc592a51634bc3ffc199
Tom-B123 <tom.h.barton@gmail.com>
2025-10-26 01:48:38 +0000

    Fixed bug of walls being generated multiple times, just need to make navmesh be a nicer shape so cows move less weirdly then its done!!!!!!!!

--- scripts/NavigationTilemap.gd
+++ scripts/NavigationTilemap.gd
@@ -11,24 +11,29 @@ var is_changed = false
 @export var wall: PackedScene
 @export var destructableItem: PackedScene
 
+var first_time = true
+
 #@export var navPolygon: NavigationPolygon
 # Code for fixing navigation for wall layers: https://www.youtube.com/watch?v=7ZAF_fn3VOc
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
+	
 	# Check if any of the wall layers overlap with this tile
 	for layer in [low_walls,high_walls]:
 		if coords in layer.get_used_cells_by_id(1):
-			var n_wall = wall.instantiate()
-			n_wall.global_position = to_global(map_to_local(coords)) + Vector2(8,8)
-			get_parent().get_parent().add_wall(n_wall)
+			if first_time:
+				var n_wall = wall.instantiate()
+				n_wall.global_position = to_global(map_to_local(coords)) + Vector2(8,8)
+				get_parent().get_parent().add_wall(n_wall)
 			is_changed = true
 			return true
 	if coords in destructible_walls.get_used_cells_by_id(1):
-		var n_wall = destructableItem.instantiate()
-		n_wall.global_position = to_global(map_to_local(coords)) + Vector2(3,3)
-		n_wall.tilePos = coords
-		n_wall.tileMap = destructible_walls
-		n_wall.navMap = self
-		get_parent().get_parent().add_wall(n_wall)
+		if first_time:
+			var n_wall = destructableItem.instantiate()
+			n_wall.global_position = to_global(map_to_local(coords)) + Vector2(3,3)
+			n_wall.tilePos = coords
+			n_wall.tileMap = destructible_walls
+			n_wall.navMap = self
+			get_parent().get_parent().add_wall(n_wall)
 		is_changed = true
 		return true
 	return false
@@ -44,6 +49,7 @@ func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
 		#tile_data.set_navigation_polygon(0, tile_data.get_navigation_polygon(0))
 func _process(delta: float) -> void:
 	if is_changed:
+		first_time = false
 		is_changed = false
 		tile_update.emit()
 		
