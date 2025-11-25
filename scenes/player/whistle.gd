1dfeac49d27c62cfadb4a8d52fd7bf9296247a94
Dahmed234 <115463196+Dahmed234@users.noreply.github.com>
2025-11-25 13:28:28 +0000

    whistle animation

--- scenes/player/whistle.gd
+++ scenes/player/whistle.gd
@@ -1,21 +1,30 @@
 extends Area2D
 
+var isWhistling = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	
+	rotation += delta 
 	
 	# move to match mouse position
 	global_position = get_global_mouse_position()
 	
 	
 	if(Input.is_action_pressed("player_whistle")):
+		isWhistling = true
+		#$whistleAnim.play("grow_whistle")
 		var throwables  = self.get_overlapping_bodies().filter(func(item): return item is Follower) #get all pikmin within area
 		throwables.map(func(item): item.onWhistle())
+	else:
+		#$whistleAnim.play("RESET")
+		isWhistling = false
 	
 	
 	#if(true):
---
+++ scenes/player/whistle_outline.gd
@@ -0,0 +1,25 @@
+extends Line2D
+
+
+@export 
+var shape :CircleShape2D
+
+@export
+var resolution = 16
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	var radius = shape.radius
+	
+	var newPointArray: Array[Vector2] = []
+	for i in resolution:
+		newPointArray.append(Vector2.from_angle(2 * PI *i /resolution)  *  radius )
+	
+	points = PackedVector2Array(newPointArray)
+	pass
