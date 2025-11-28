6405c10916d66151235ac17fe158cadf23064fde
Feranmi Akodu <feranmi2005dev@gmail.com>
2025-11-28 13:25:28 +0000

    particles only spawn when min followewrs

--- scenes/items/destructables/particles.gd
+++ scenes/items/destructables/particles.gd
@@ -3,9 +3,11 @@ extends GPUParticles2D
 func _ready() -> void:
 	emitting = false
 	
-func _on_destructable_item_carry_started(follower: CharacterBody2D) -> void:
-	emitting = true # Replace with function body.
+func _on_destructable_item_carry_started(follower: CharacterBody2D, num_followers: int, min_followers: int, max_followers: int) -> void:
+	if (num_followers >= min_followers):
+		emitting = true # Replace with function body.
 
 
-func _on_destructable_item_carry_stopped(follower: CharacterBody2D) -> void:
-	emitting = false # Replace with function body.
+func _on_destructable_item_carry_stopped(follower: CharacterBody2D, num_followers: int, min_followers: int, max_followers: int) -> void:
+	if (num_followers < min_followers):
+		emitting = false # Replace with function body.
