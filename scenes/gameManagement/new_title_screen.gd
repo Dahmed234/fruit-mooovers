a4eb06268aa104362302f564c064b7b6b646c824
Dahmed234 <115463196+Dahmed234@users.noreply.github.com>
2025-12-08 22:17:33 +0000

    new title screen!!!

---
+++ scenes/gameManagement/new_title_screen.gd
@@ -0,0 +1,36 @@
+extends Node2D
+
+@export
+var followerScene : PackedScene
+
+@export
+var game_scene : PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#_on_timer_timeout()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_timer_timeout() -> void:
+	$Timer.start()
+	var newFollower :Node2D = followerScene.instantiate()
+	newFollower.global_position = $CanvasLayer/spawnPoint.global_position
+	$CanvasLayer.add_child(newFollower)
+	pass # Replace with function body.
+
+
+func _start_game() -> void:
+	var in_game = game_scene.instantiate()
+	#get_tree().change_scene_to_packed(in_game)
+	get_tree().root.add_child(in_game)
+	queue_free()
+
+
+func _on_button_2_pressed() -> void:
+	get_tree().quit()
