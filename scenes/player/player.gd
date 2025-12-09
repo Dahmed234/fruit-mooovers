83ebb3e7d713de77764bb3c841d1c192b8eb6fec
Tom-B123 <tom.h.barton@gmail.com>
2025-12-09 03:42:59 +0000

    more confusion, trying to find what makes the game freeze up rather than crashing like a normal person

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -32,7 +32,9 @@ func die() -> void:
 	for cone_light in chasing:
 		if !chasing[cone_light]: continue
 		cone_light.clear_target(self)
-		
+	
+	print("player die!!!!")
+	
 	playerDies.emit()
 
 func getThrowPosition():
