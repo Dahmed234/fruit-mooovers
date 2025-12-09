1cb22a38d8f7ef941767233d54d973ecfc3f7803
Tom-B123 <tom.h.barton@gmail.com>
2025-12-09 15:23:28 +0000

    paths updating properly when destroying

--- scenes/maps/navigation_region_2d.gd
+++ scenes/maps/navigation_region_2d.gd
@@ -9,8 +9,8 @@ func rebake():
 	bake_navigation_polygon(true)
 	
 func _ready() -> void:
-	#get_tree().get_nodes_in_group("destroyableWall").map(func(i) :
-			#i.requestDestroy.connect(_on_object_destroyed))
+	get_tree().get_nodes_in_group("destroyableWall").map(func(i) :
+			i.requestDestroy.connect(_on_object_destroyed))
 
 	#navMap.notify_runtime_tile_data_update()
 	pass
